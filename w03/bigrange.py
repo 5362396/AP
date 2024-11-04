f3d17434cb5cd4f8af7e10cad2698403d0afbb16
5362396 <56536893+5362396@users.noreply.github.com>
2024-11-04 14:58:37 +0100

    lab 03 exercises

---
+++ w03/bigrange.py
@@ -0,0 +1,22 @@
+import sys
+import timeit
+import psutil
+
+
+def main(n):
+    total_sum = sum(range(1, n + 1))
+    print(f'Suma liczb od 1 do {n} wynosi: {total_sum}')
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        n = int(sys.argv[1])
+    else:
+        print('Podaj wartość n jako argument programu.')
+        sys.exit(1)
+
+    time_taken = timeit.timeit(lambda: main(n), number=1)
+    process = psutil.Process()
+    memory_usage_kb = process.memory_info().rss / 1024
+
+    print(f'{time_taken:.2f} sec. {memory_usage_kb:.2f} kB')
