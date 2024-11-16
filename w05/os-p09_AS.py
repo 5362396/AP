d4e6001636eba185020e9d02059a276ee23a59a4
aras2137 <5362396@wp.pl>
2024-11-16 18:44:13 +0100

    Lab05 add comment

--- w05/os-p09_AS.py
+++ w05/os-p09_AS.py
@@ -17,3 +17,13 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+# (.venv) PS C:\Users\Arek\PycharmProjects\AP\w05> python os-p09_AS.py
+# Głowa i ogon bieżącego katalogu:
+# ('C:\\Users\\Arek\\PycharmProjects\\AP', 'w05')
+#         Korzeń oraz rozszerzenie ścieżki ../app-2024-w5.pdf
+# ('app-2024-w5', '.pdf')
+#         Ścieżka bezwzględna dla katalogu bieżącego:
+# C:\Users\Arek\PycharmProjects\AP\w05
+#         Ścieżka bezwzględna dla katalogu nadrzędnego:
+# C:\Users\Arek\PycharmProjects\AP
